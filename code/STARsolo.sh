f3540f85ab5060abd13dacff57bca67d4fcdc166
Simone Zaghen <zaghen@chalmers.se>
2024-01-17 17:55:04 -0500

    added some comments to clarify code

--- code/STARsolo.sh
+++ code/STARsolo.sh
@@ -152,10 +152,11 @@ rule find_peaks:
         """
             macs3 callpeak -t {input} \
                 --name {wildcards.sample} \
-                --gsize 1.2e7 \
-                --nomodel \
-                --shift -75 \
-                --extsize 150 \
+                --gsize 1.2e7 `#mappable genome size, defined as the genome size which can be sequenced.` \
+                --nomodel `#turn off model construction`\
+                --shift -75 `#extend reads from 5’ to 3’ `\
+                --extsize -150 `#extend reads in 5’->3’ direction to fix-sized fragments`\
+                --max-gap 10 `#max gap between regions`\
                 --outdir {params.outdir}
 
         """
